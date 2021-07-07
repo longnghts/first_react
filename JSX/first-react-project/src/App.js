f520fffe0760f2851c7689009f289e94d4555b1a
longnghts <82634370+longnghts@users.noreply.github.com>
2021-07-07 19:41:06 -0400

    Init React Commit

---
+++ JSX/first-react-project/src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <h1>Hello World!</h1>
+      <h3>Things that i have to do.</h3>
+      <ul>
+        <li>Learn React</li>
+        <li>Climb Mt. Everest</li>
+        <li>Run a marathon</li>
+        <li>Feed the dogs</li>
+      </ul>
+    </div>
+  );
+}
+
+export default App;
+
